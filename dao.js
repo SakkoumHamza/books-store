4ab80ef3d5611bcb97a9b64cf0faf95d8c78b3b9
sakkoumhamza <hamzasakkoum1@gmail.com>
2025-09-21 19:17:29 +0100

    Added book schema and MongoDb connection logic

---
+++ dao.js
@@ -0,0 +1,23 @@
+const Mongoose = require('mongoose')
+ 
+const bookSchema = new Mongoose.Schema({
+  id: String,
+  title: String,
+  cover: String,
+  publishDate: String,
+  rating: String,
+  author: String,
+  description: String
+})
+
+module.exports = {
+  init: () => {
+    return Mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log('✅ Successfully connected to MongoDB'))
+      .catch(err => console.error('❌ MongoDB connection error:', err));
+  },
+  close: () => Mongoose.disconnect(),
+  Book: Mongoose.model('books', bookSchema),
+  Favorite: Mongoose.model('favorite', bookSchema),
+}
+
